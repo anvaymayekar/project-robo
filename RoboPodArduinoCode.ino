3f81c42fcbcffd7c4a661349f9c14dc42cc8e2e7
Anvay Mayekar <74885795+anvaymayekar@users.noreply.github.com>
2025-07-04 12:45:00 +0530

    Refurbished RoboPodArduinoCode.ino

--- RoboPodArduinoCode.ino
+++ RoboPodArduinoCode.ino
@@ -1,10 +1,9 @@
-
-// author: @anvaymayekar
-
 #include <SoftwareSerial.h>
 #include <NewPing.h>
 
-SoftwareSerial BT(2, 3); // RX, TX
+// author: @anvaymaykar
+
+SoftwareSerial BT(2, 3);
 
 const int M1_F = 9;
 const int M1_R = 10;
@@ -13,157 +12,157 @@ const int M2_R = 12;
 
 const int trigPin = 4;
 const int echoPin = 5;
-
-const int max = 400;
+const int MAX_DISTANCE = 400;
 
 const int rightIR = A1;
 const int leftIR = A2;
 
-int incommingByte;
+const int OBSTACLE_THRESHOLD = 20;
+const float IR_SCALING = 0.01;
+const int TURN_DELAY = 400;
+const int MOTOR_DELAY = 100;
+
+int incomingByte;
 int distance = 100;
-
 bool autopilot = false;
-NewPing sonar(trigPin, echoPin, max);
 
-//---------------------------------------
+NewPing sonar(trigPin, echoPin, MAX_DISTANCE);
 
-void setup()
-{
+// Function prototypes
+void setup();
+void loop();
+void stop();
+void forward();
+void backward();
+void right();
+void left();
+void handleManualCommand(char cmd);
+void autopilotMode();
+int getDistance();
+
+void setup() {
   pinMode(M1_F, OUTPUT);
   pinMode(M1_R, OUTPUT);
   pinMode(M2_F, OUTPUT);
   pinMode(M2_R, OUTPUT);
 
-
-
   pinMode(rightIR, INPUT);
   pinMode(leftIR, INPUT);
 
-
   Serial.begin(9600);
   BT.begin(9600);
+  stop();
 }
 
-//---------------------------------------
+void loop() {
+  if (BT.available()) {
+    incomingByte = BT.read();
 
-int getDistance() {
-    return sonar.ping_cm();
-}
-
-void autopilotMode() {
-    int distance = getDistance();
-    float rightSensor = analogRead(rightIR) * 0.01;
-    float leftSensor = analogRead(leftIR) * 0.01; 
-    
-    if (distance < 20) {
-      Serial.println(distance);
-      Serial.println(rightSensor);
-      Serial.println(leftSensor);
-
-        stop();
-        delay(500);
-        backward();
-        delay(500);
-        if (leftSensor < rightSensor) {
-            right();
-            delay(400);
-     
-        } else if (rightSensor < leftSensor) {
-            left();
-            delay(400);
-
-        } else {
-            right();
-            delay(400);
-
-        }
-        forward();
-    } else {
-        forward();
-    }
-}
-void loop()
-{
-  if (BT.available() > 0)
-  {
-    incommingByte = BT.read();
-    if (incommingByte == 'A'){
+    if (incomingByte == 'A') {
       autopilot = true;
+    } else if (incomingByte == 'S') {
+      autopilot = false;
+      stop();
     } else {
       autopilot = false;
-      switch (incommingByte)
-      {
-        case 'F': forward();
-          break;
-        case 'B': backward();
-          break;
-        case 'S': stop();
-          break;
-        case 'R': right();
-          break;
-        case 'L': left();
-          break;
-
-      }
+      handleManualCommand(incomingByte);
     }
   }
 
-
- while (autopilot) {
-  incommingByte = BT.read();
-    if (incommingByte == 'S'){
-      autopilot = false;
-    }
+  if (autopilot) {
     autopilotMode();
+  }
+}
+
+int getDistance() {
+  return sonar.ping_cm();
+}
+
+void autopilotMode() {
+  distance = getDistance();
+  float rightSensor = analogRead(rightIR) * IR_SCALING;
+  float leftSensor = analogRead(leftIR) * IR_SCALING;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" | Right IR: ");
+  Serial.print(rightSensor);
+  Serial.print(" | Left IR: ");
+  Serial.println(leftSensor);
+
+  if (distance > 0 && distance < OBSTACLE_THRESHOLD) {
+    stop();
+    delay(500);
+    backward();
+    delay(500);
+
+    if (leftSensor < rightSensor) {
+      right();
+      delay(TURN_DELAY);
+    } else if (rightSensor < leftSensor) {
+      left();
+      delay(TURN_DELAY);
+    } else {
+      right();
+      delay(TURN_DELAY);
     }
 
-  
+    forward();
+  } else {
+    forward();
+  }
 }
-//-----------------------------------------------
 
-void stop()
-{
+void handleManualCommand(char cmd) {
+  switch (cmd) {
+    case 'F': forward(); break;
+    case 'B': backward(); break;
+    case 'R': right(); break;
+    case 'L': left(); break;
+    case 'S': stop(); break;
+  }
+}
+
+void stop() {
   digitalWrite(M1_F, LOW);
   digitalWrite(M1_R, LOW);
   digitalWrite(M2_F, LOW);
   digitalWrite(M2_R, LOW);
-  delay(100);
+  delay(MOTOR_DELAY);
 }
-void forward()
-{
+
+void forward() {
   BT.println("FORWARD");
   digitalWrite(M1_F, HIGH);
   digitalWrite(M1_R, LOW);
   digitalWrite(M2_F, HIGH);
   digitalWrite(M2_R, LOW);
-delay(100);
+  delay(MOTOR_DELAY);
 }
-void backward()
-{
+
+void backward() {
   BT.println("BACKWARD");
   digitalWrite(M1_F, LOW);
   digitalWrite(M1_R, HIGH);
   digitalWrite(M2_F, LOW);
   digitalWrite(M2_R, HIGH);
-delay(100);
+  delay(MOTOR_DELAY);
 }
-void right()
-{
+
+void right() {
   BT.println("RIGHT");
   digitalWrite(M1_F, LOW);
   digitalWrite(M1_R, HIGH);
   digitalWrite(M2_F, HIGH);
   digitalWrite(M2_R, LOW);
-  delay(100);
+  delay(MOTOR_DELAY);
 }
-void left()
-{
+
+void left() {
   BT.println("LEFT");
   digitalWrite(M1_F, HIGH);
   digitalWrite(M1_R, LOW);
   digitalWrite(M2_F, LOW);
   digitalWrite(M2_R, HIGH);
-  delay(100);
+  delay(MOTOR_DELAY);
 }
-
-
